e7e0e434530f5165da1ccc4263c12926313defa7
cMyc <Myc.Chiu@gmail.com>
2014-02-18 06:12:08 +0000

    added node.js

--- web.js
+++ web.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
-var buffer = new Buffer(fs.readFile('index.html'));
+var index = new Buffer(fs.readFile('index.html'));
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString);
+  response.send(index.toString);
+});
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
