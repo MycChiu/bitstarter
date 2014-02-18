de45e909cb97059333317badc6b17d5a6e1910af
cMyc <Myc.Chiu@gmail.com>
2014-02-18 06:48:44 +0000

    mod web.js

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var fs = require('fs');
 var app = express();
-var index = new Buffer(fs.readFileSync('index.html'));
+var index = fs.readFileSync('index.html');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(index.toString);
+  response.send(index.toString());
 });
 
 
