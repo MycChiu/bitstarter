2d237211f35c89c5ef82a6c0ae0df5fb8eedc2d9
cMyc <Myc.Chiu@gmail.com>
2014-02-18 05:52:16 +0000

    mod web.js

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
+var buffer = new Buffer(fs.readFile('index.html'))
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+  response.send(buf.toString);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
